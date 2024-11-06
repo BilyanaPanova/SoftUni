48ead36b69177de6977997d588603ea0eab99d1a
Bilyana Panova <156317102+BilyanaPanova@users.noreply.github.com>
2024-11-06 19:54:27 +0200

    Update sorting_number2.js

--- JS Frond-End/Arrays and Strings/Exercises/04/sorting_number2.js
+++ JS Frond-End/Arrays and Strings/Exercises/04/sorting_number2.js
@@ -1,12 +1,12 @@
+
 function sorting(arr) {
     arr.sort((a,b) => a-b)
     let result = [];
 
     while (arr.length > 0) {
-        result.push(arr.shift())
-        if (arr.length > 0) 
-            result.push(arr.pop())
+        result.push(arr.shift(),arr.pop())
     }
 
     return result
 }
+
