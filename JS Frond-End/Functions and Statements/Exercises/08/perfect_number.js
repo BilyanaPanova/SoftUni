e6332f12898d3e302028675051b4475f3d662943
Bilyana Panova <156317102+BilyanaPanova@users.noreply.github.com>
2024-11-08 14:53:42 +0200

    Create perfect_number.js

---
+++ JS Frond-End/Functions and Statements/Exercises/08/perfect_number.js
@@ -0,0 +1,19 @@
+function isPerfectNumber(number) {
+    let sum = 0; for (let i = 1; i < number; i++) if (number % i === 0) sum += i;
+    sum === number ? console.log("We have a perfect number!") : console.log("It's not so perfect.");
+
+}
+
+//or
+
+function isPerfectNumber(number) {
+    let sum = 0;
+    for (let i = 1; i < number; i++) {
+        if (number % i === 0) {
+            sum += i;
+        }
+    }
+    
+    sum === number ? console.log("We have a perfect number!") : console.log("It's not so perfect.");
+
+}
