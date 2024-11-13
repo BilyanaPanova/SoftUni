6c39447ff153ae86d64d089dd1bc0cabfa984f39
Bilyana Panova <156317102+BilyanaPanova@users.noreply.github.com>
2024-11-13 18:32:19 +0200

    Update employees.js

--- JS Frond-End/Objects and Classes/Exercises/01/employees.js
+++ JS Frond-End/Objects and Classes/Exercises/01/employees.js
@@ -1,10 +1,10 @@
-function employeersInformation(arrNames) {
-    let objEmployeers = arrNames.reduce((acc, empl) => {
+function employeesInformation(arrNames) {
+    let objEmployees = arrNames.reduce((acc, empl) => {
         acc[empl] = empl.length;
         return acc;
     }, {});
 
-    for (let [name,value] of Object.entries(objEmployeers)) {
+    for (let [name,value] of Object.entries(objEmployees)) {
     console.log(`Name: ${name} -- Personal Number: ${value}`)
     }
 
