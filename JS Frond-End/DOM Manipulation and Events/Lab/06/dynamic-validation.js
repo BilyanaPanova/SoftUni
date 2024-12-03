e2c00f3bfe1d3c8c2bf22d9ac99d01bdfb7a5d6c
Bilyana Panova <156317102+BilyanaPanova@users.noreply.github.com>
2024-12-03 12:17:51 +0200

    Create dynamic-validation.js

---
+++ JS Frond-End/DOM Manipulation and Events/Lab/06/dynamic-validation.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', solve);
+
+function solve() {
+    const emailInput = document.querySelector('#email'); 
+
+    emailInput.addEventListener('change', () => {
+        const emailPattern = /^[a-z]+@[a-z]+\.[a-z]+$/; 
+        if (!emailPattern.test(emailInput.value)) {
+            emailInput.classList.add('error');
+        } else {
+            emailInput.classList.remove('error');
+        }
+    });
+}
