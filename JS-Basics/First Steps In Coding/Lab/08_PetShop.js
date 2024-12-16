82cb2a93183cc49e3e78ce7022d0a434ab1fb837
Bilyana Panova <156317102+BilyanaPanova@users.noreply.github.com>
2024-12-16 18:53:43 +0200

    Create 08_PetShop.js

---
+++ JS-Basics/First Steps In Coding/Lab/08_PetShop.js
@@ -0,0 +1,10 @@
+function petShop(dogPackages,catPackages) {
+
+    const priceOneDogPackage = 2.50
+    const priceOneCatPackage = 4
+
+    const result = (dogPackages * priceOneDogPackage) + (catPackages * priceOneCatPackage)
+
+    console.log(`${result} lv.`)
+    
+}
