6040b225f34e3bd3f4561649f67374bbdd1a0b5d
Bilyana Panova <156317102+BilyanaPanova@users.noreply.github.com>
2024-12-16 18:46:09 +0200

    Create 07_ProjectsCreation.js

---
+++ JS-Basics/First Steps In Coding/Lab/07_ProjectsCreation.js
@@ -0,0 +1,7 @@
+function projectCreation(architect,projects) {
+    const timeToFinishOneProject = 3
+
+    const result = projects * timeToFinishOneProject
+
+    console.log(`The architect ${architect} will need ${result} hours to complete ${projects} project/s.`)
+}
