dfff225f77d4df1640b24c5e9e5b77d396b83528
Bilyana Panova <156317102+BilyanaPanova@users.noreply.github.com>
2024-12-20 11:28:38 +0200

    Create 01.js

---
+++ JS-Basics/Conditional Statements Advanced/Lab/01.js
@@ -0,0 +1,5 @@
+function solve(number) {
+    const dayOfTheWeek = {1:"Monday",2:"Tuesday",3:"Wednesday",4:"Thursday",5:"Friday",6:"Saturday",7:"Sunday"}
+
+    console.log(dayOfTheWeek[number] || "Error");
+}
