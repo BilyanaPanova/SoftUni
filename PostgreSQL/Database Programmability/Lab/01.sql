282716fa6bfac9ee6359b87d5435768ac1268099
Bilyana Panova <156317102+BilyanaPanova@users.noreply.github.com>
2025-01-31 17:04:57 +0200

    Update 01.sql

--- PostgreSQL/Database Programmability/Lab/01.sql
+++ PostgreSQL/Database Programmability/Lab/01.sql
@@ -18,3 +18,18 @@ BEGIN
 END;
 $$
 LANGUAGE plpgsql;
+
+
+--OR 
+
+CREATE OR REPLACE FUNCTION fn_count_employees_by_town(IN town_name VARCHAR(20),OUT e_count INT)
+AS $$
+BEGIN 
+	SELECT 
+		COUNT (*) INTO e_count
+	FROM employees AS e
+	JOIN addresses AS a ON a.address_id = e.address_id
+	JOIN towns AS t ON t.town_id = a.town_id
+	WHERE t.name = town_name;
+END;
+$$ LANGUAGE plpgsql;
