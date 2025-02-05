3517269a1ec890689336f46fee071ec48b7bd2ce
Bilyana Panova <156317102+BilyanaPanova@users.noreply.github.com>
2025-02-05 15:36:16 +0200

    Update 10.sql

--- PostgreSQL/Exams/Soccer Talent/10.sql
+++ PostgreSQL/Exams/Soccer Talent/10.sql
@@ -12,3 +12,29 @@ BEGIN
     WHERE CONCAT(p.first_name, ' ', p.last_name) = player_name;
 END;
 $$;
+
+
+--OR
+
+CREATE OR REPLACE PROCEDURE sp_players_team_name(
+    IN player_name VARCHAR(50),
+    OUT team_name VARCHAR(100)
+)
+LANGUAGE plpgsql
+AS $$
+DECLARE 
+    team_id_n INT;
+BEGIN
+    SELECT team_id INTO team_id_n
+    FROM players
+    WHERE CONCAT(first_name, ' ', last_name) = player_name;
+
+    IF team_id_n IS NULL THEN
+        team_name := 'The player currently has no team';
+    ELSE
+        SELECT name INTO team_name
+        FROM teams
+        WHERE id = team_id_n;
+    END IF;
+END;
+$$;
